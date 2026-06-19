3d2b01ea789440dc14796c3309b6ae09a154cbcf
ytihianine <ytihianine.pro@gmail.com>
2026-06-20 00:00:25 +0200

    fix(argocd/superset): update config override to fix whitescreen issue

--- argocd/superset/superset_config_override.py
+++ argocd/superset/superset_config_override.py
@@ -37,6 +37,16 @@ SQLALCHEMY_ENGINE_OPTIONS = {
 }
 
 # ------------------------
+# Langues
+# ------------------------
+BABEL_DEFAULT_LOCALE = "fr"
+
+LANGUAGES = {
+    "fr": {"flag": "fr", "name": "Français"},
+    "en": {"flag": "us", "name": "English"},
+}
+
+# ------------------------
 # Thèmes
 # ------------------------
 FAVICONS = [{"href": "/static/assets/local/images/app_icon.png"}]
