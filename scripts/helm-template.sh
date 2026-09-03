399961b66e5c04531f1dfb346d5fb612c930c290
ytihianine <ytihianine.pro@gmail.com>
2026-09-03 23:40:31 +0200

    chore(ci): remove chart rendering from logs

--- scripts/helm-template.sh
+++ scripts/helm-template.sh
@@ -29,12 +29,11 @@ cleanup() {
 trap cleanup EXIT
 
 for chart in "${charts[@]}"; do
-  echo "==> Rendering $chart"
-  helm dependency build "$chart"
+  helm dependency build "$chart" >/dev/null
   values_file="$chart/values-prod.yaml"
   if [ -f "$values_file" ]; then
-    helm template "render-${chart//\//}" "$chart" -f "$values_file" --namespace default
+    helm template "render-${chart//\//}" "$chart" -f "$values_file" --namespace default >/dev/null
   else
-    helm template "render-${chart//\//}" "$chart" --namespace default
+    helm template "render-${chart//\//}" "$chart" --namespace default >/dev/null
   fi
 done
